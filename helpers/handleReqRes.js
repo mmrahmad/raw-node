b6f8e9a1b9ee6324088e5306174e62c0ade68388
Ahmad <info@mmrahmad.com>
2022-06-23 17:17:47 +0600

    feat(helpers): add handle req res

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,70 @@
+/**
+ * Title: Application handler
+ * Description: This is application handler to handle req-res
+ * Author: MMR Ahmad
+ * Data: 23-06-2022
+ */
+
+// Dependencies
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+const routes = require('../routes');
+const {
+  notFoundHandler,
+} = require('../handlers/routeHandlers/notFoundHandler');
+
+// Module scaffolding
+const handler = {};
+
+// handlers
+handler.handleReqRes = (req, res) => {
+  // handle the request
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const method = req.method.toLowerCase();
+  const queryObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const reqProperties = {
+    parsedUrl,
+    path,
+    trimmedPath,
+    method,
+    queryObject,
+    headersObject,
+  };
+
+  console.log(reqProperties);
+
+  // chosenHandler
+  const chosenHandler = routes[trimmedPath]
+    ? routes[trimmedPath]
+    : notFoundHandler;
+  chosenHandler(reqProperties, (statusCode, payload) => {
+    statusCode = typeof statusCode === 'number' ? statusCode : 500;
+    payload = typeof payload === 'object' ? payload : {};
+
+    const payloadString = JSON.stringify(payload);
+
+    // return final response
+    res.writeHead(statusCode);
+    res.end(payloadString);
+  });
+
+  //   Decoder
+  const decoder = new StringDecoder('utf-8');
+  let realString = '';
+  //   Listen buffer data
+  req.on('data', (buffer) => {
+    realString += decoder.write(buffer);
+  });
+  req.on('end', () => {
+    realString += decoder.end();
+    console.log('handleReqRes: ', realString);
+    res.end(realString);
+  });
+};
+
+module.exports = handler;
