58b663a599986db8dd42ac95cb8ca723de662613
Ahmad <info@mmrahmad.com>
2022-06-23 18:47:07 +0600

    feat(handler/routerHandler): add get, put & delete method

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -6,7 +6,7 @@
  */
 
 // Dependencies
-const { hash } = require('../../helpers/utilities');
+const { hash, parseJson } = require('../../helpers/utilities');
 const data = require('../../lib/data');
 
 // Module scaffolding
@@ -25,9 +25,29 @@ handler.userHandler = (reqProperties, callback) => {
 handler._users = {};
 
 handler._users.get = (reqProperties, callback) => {
-  callback(200, {
-    data: 'This is data',
-  });
+  queryMobile = reqProperties.queryObject?.mobile;
+  mobile =
+    typeof queryMobile === 'string' &&
+    queryMobile.trim().length === 11
+      ? queryMobile
+      : null;
+  if (mobile) {
+    data.read('users', mobile, (err, user) => {
+      const userCopy = { ...parseJson(user) };
+      if (!err && userCopy) {
+        delete userCopy.password;
+        callback(200, userCopy);
+      } else {
+        callback(404, {
+          error: 'Requested user not found!',
+        });
+      }
+    });
+  } else {
+    callback(404, {
+      message: 'Query mobile not found!',
+    });
+  }
 };
 handler._users.post = (reqProperties, callback) => {
   // REQ body validation
@@ -51,18 +71,30 @@ handler._users.post = (reqProperties, callback) => {
     reqProperties.body.mobile.trim().length === 11
       ? reqProperties.body.mobile
       : null;
+  email =
+    typeof reqProperties.body.email === 'string' &&
+    reqProperties.body.email.trim().length > 0
+      ? reqProperties.body.email
+      : null;
   tscAccept =
     typeof reqProperties.body.tscAccept === 'boolean'
       ? reqProperties.body.tscAccept
       : null;
 
-  if (firstName && lastName && mobile && password && tscAccept) {
+  if (
+    firstName &&
+    lastName &&
+    mobile &&
+    password &&
+    email &&
+    tscAccept
+  ) {
     data.read('users', mobile, (err, user) => {
       if (err) {
-        // TODO: Create user data
         const userData = {
           firstName,
           lastName,
+          email,
           mobile,
           password: hash(password),
           tscAccept,
@@ -86,12 +118,102 @@ handler._users.post = (reqProperties, callback) => {
     });
   } else {
     callback(400, {
-      error: reqProperties?.body,
+      error: 'There is an problem in your request!',
     });
   }
 };
-handler._users.put = (reqProperties, callback) => {};
-handler._users.delete = (reqProperties, callback) => {};
+handler._users.put = (reqProperties, callback) => {
+  // REQ body validation
+  firstName =
+    typeof reqProperties.body.firstName === 'string' &&
+    reqProperties.body.firstName.trim().length > 0
+      ? reqProperties.body.firstName
+      : null;
+  lastName =
+    typeof reqProperties.body.lastName === 'string' &&
+    reqProperties.body.lastName.trim().length > 0
+      ? reqProperties.body.lastName
+      : null;
+  password =
+    typeof reqProperties.body.password === 'string' &&
+    reqProperties.body.password.trim().length > 0
+      ? reqProperties.body.password
+      : null;
+  mobile =
+    typeof reqProperties.body.mobile === 'string' &&
+    reqProperties.body.mobile.trim().length === 11
+      ? reqProperties.body.mobile
+      : null;
+  email =
+    typeof reqProperties.body.email === 'string' &&
+    reqProperties.body.email.trim().length > 0
+      ? reqProperties.body.email
+      : null;
+
+  if (firstName || lastName || password || email) {
+    data.read('users', mobile, (err, userData) => {
+      const userCopy = { ...parseJson(userData) };
+      if (!err && userCopy) {
+        if ('firstName') {
+          userCopy.firstName = firstName;
+        }
+        if ('lastName') {
+          userCopy.lastName = lastName;
+        }
+        if ('email') {
+          userCopy.email = email;
+        }
+        if ('password') {
+          userCopy.password = hash(password);
+        }
+        data.update('users', mobile, userCopy, (err) => {
+          if (err) {
+            callback(500, {
+              error: 'There was an server side error!',
+            });
+          } else {
+            callback(200, {
+              message: 'User update successful!',
+            });
+          }
+        });
+      } else {
+        callback(500, {
+          error: 'User not exist!',
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: 'There is an problem in your request!',
+    });
+  }
+};
+handler._users.delete = (reqProperties, callback) => {
+  queryMobile = reqProperties.queryObject?.mobile;
+  mobile =
+    typeof queryMobile === 'string' &&
+    queryMobile.trim().length === 11
+      ? queryMobile
+      : null;
+  if (mobile) {
+    data.delete('users', mobile, (err) => {
+      if (!err) {
+        callback(200, {
+          message: 'User delete successful!',
+        });
+      } else {
+        callback(404, {
+          error: 'Requested user not found!',
+        });
+      }
+    });
+  } else {
+    callback(404, {
+      message: 'Query mobile not found!',
+    });
+  }
+};
 
 // Export
 module.exports = handler;
